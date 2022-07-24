ad9c86704323345dfc622a111eaae688a2a7497b
Chidobe <thedobecompany@gmail.com>
2022-07-24 07:06:41 +0100

    dom manipulation exercise completed

--- dom.js
+++ dom.js
@@ -3,10 +3,16 @@ const container = document.querySelector('#container');
 const para = document.createElement('p');
 para.textContent = "Hey i'm red!";
 para.setAttribute('style', 'color:red;');
-container.appendChild(para);
+//container.appendChild(para);
 
 const heading = document.createElement('h3');
 heading.textContent = "I'm a blue h3!";
 heading.style.cssText = "color: blue;";
-container.insertBefore(heading, para);
+//container.insertBefore(heading, para);
 
+const newDiv = document.createElement('div');
+newDiv.innerHTML = "<h1 style='color:green'>I'm in a div</h1> <p>me too</p>" ;
+newDiv.setAttribute('style', 'background-color: pink; border: 2px solid black;');
+
+/* i am appending all elements at once instead of one after the other */
+container.append(para, heading, newDiv);
